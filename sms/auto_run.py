2a7a86ea8317ea8fb87fd2077740de84e73ac3e9
Cash <cashhilstad@gmail.com>
2025-04-26 14:11:18 -0700

    tweaks

--- sms/auto_run.py
+++ sms/auto_run.py
@@ -3,10 +3,11 @@ import time
 import subprocess
 
 def run_scraper():
-    subprocess.run(["python3", "/Users/jadencampbell/someone_pick_a_name/sms/scrape_menu.py"])
+    subprocess.run(["python3", "./sms/scrape_menu.py"])
 
 # Run every day at 6:00 AM
 schedule.every().day.at("06:00").do(run_scraper)
+run_scraper()
 
 while True:
     schedule.run_pending()
